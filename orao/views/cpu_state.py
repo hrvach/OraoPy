b6d31d994c0957087954043ed3df70caa0495c13
shokre <kbrlek@gmail.com>
2021-11-25 02:42:40 +0100

    fix: fix stack pointer address

--- orao/views/cpu_state.py
+++ orao/views/cpu_state.py
@@ -20,7 +20,7 @@ class CPUState(View):
 		self.draw_text(0, 2 * 8, 'PC:', color=lc)
 		self.draw_text(8 * 8, 2 * 8, 'SP:', color=lc)
 		self.draw_text(3 * 8, 2 * 8, '%04X' % cpu.pc)
-		self.draw_text(11 * 8, 2 * 8, '%04X' % cpu.sp)
+		self.draw_text(11 * 8, 2 * 8, '%04X' % (cpu.sp + 0x100))
 
 		self.draw_text(8 * 8, 0 * 8, "NVssDIZC", color=lc)
 		self.draw_text(8 * 8, 1 * 8, "{0:b}".format(cpu.flags))
